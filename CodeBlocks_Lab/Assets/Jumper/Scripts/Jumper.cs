0f5ee5aa4b2db637190197f4e6e5ff7653de20cb
Nomad <netherchef@gmail.com>
2020-05-02 02:54:57 +0800

    Add [Jumper]

---
+++ CodeBlocks_Lab/Assets/Jumper/Scripts/Jumper.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jumper : MonoBehaviour
+{
+	// Components
+
+	public Rigidbody2D subject;
+
+	// Variables
+
+	public float jumpVelocity = 5;
+
+	public float fallMultiplier = 2.5f;
+	public float lowJumpMultiplier = 2;
+
+	private void Update ()
+	{
+		if (Input.GetButtonDown ("Jump"))
+		{
+			subject.velocity = new Vector2 (subject.velocity.x, 1 * jumpVelocity);
+		}
+	}
+
+	private void FixedUpdate ()
+	{
+		if (subject.velocity.y < 0)
+		{
+			subject.velocity += new Vector2 (0, 1) * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+		}
+		else if (subject.velocity.y > 0 && !Input.GetButton ("Jump"))
+		{
+			subject.velocity += new Vector2 (0, 1) * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+		}
+	}
+}
