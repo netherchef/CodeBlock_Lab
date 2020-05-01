e04e609a2f54ca162336362ba218631009867c53
Nomad <netherchef@gmail.com>
2020-05-02 02:15:42 +0800

    Add [Image Fader & Sprite Fader]

---
+++ CodeBlocks_Lab/Assets/Fader/Scripts/ImageFader.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageFader : MonoBehaviour
+{
+	// Components
+
+	public Image image;
+
+	// Variables
+
+	public bool active;
+
+	public bool fadeOut;
+	public bool fadeIn;
+
+	public float speed = 1;
+
+	private void Update ()
+	{
+		if (active)
+		{
+			if (fadeOut)
+			{
+				image.color -= new Color (0, 0, 0, speed * Time.deltaTime);
+
+				return;
+			}
+			else if (fadeIn)
+			{
+				image.color += new Color (0, 0, 0, speed * Time.deltaTime);
+
+				return;
+			}
+		}
+	}
+}
---
+++ CodeBlocks_Lab/Assets/Fader/Scripts/SpriteFader.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteFader : MonoBehaviour
+{
+	// Components
+
+	public SpriteRenderer SR;
+
+	// Variables
+
+	public bool active;
+
+	public bool fadeOut;
+	public bool fadeIn;
+
+	public float speed = 1;
+
+	private void Update ()
+	{
+		if (active)
+		{
+			if (fadeOut)
+			{
+				SR.color -= new Color (0, 0, 0, speed * Time.deltaTime);
+
+				return;
+			}
+			else if (fadeIn)
+			{
+				SR.color += new Color (0, 0, 0, speed * Time.deltaTime);
+
+				return;
+			}
+		}
+	}
+}
