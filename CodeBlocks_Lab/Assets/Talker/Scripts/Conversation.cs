95ab37fb93ac096643cae3c8923ebebf0ddacba3
Nomad <netherchef@gmail.com>
2020-05-02 23:30:09 +0800

    Add [Talker]

---
+++ CodeBlocks_Lab/Assets/Talker/Scripts/Conversation.cs
@@ -0,0 +1,10 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (fileName = "Conversation", menuName = "Conversation")]
+public class Conversation : ScriptableObject
+{
+	public string[] lines;
+}
---
+++ CodeBlocks_Lab/Assets/Talker/Scripts/Talker.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Talker : MonoBehaviour
+{
+	// Components
+
+	public Text textDisplay;
+	public Conversation conversation;
+
+	// Variables
+
+	public bool talk;
+	public bool proceed;
+
+	// Enumerators
+
+	private IEnumerator converse;
+
+	private void Start ()
+	{
+		converse = Converse ();
+		StartCoroutine (converse);
+	}
+
+	private IEnumerator Converse ()
+	{
+		while (enabled)
+		{
+			if (talk)
+			{
+				for (int i = 0; i < conversation.lines.Length; i++)
+				{
+					textDisplay.text = conversation.lines[i];
+
+					while (!proceed) yield return null;
+
+					proceed = false;
+				}
+
+				textDisplay.text = "";
+
+				talk = false;
+			}
+
+			yield return null;
+		}
+	}
+}
