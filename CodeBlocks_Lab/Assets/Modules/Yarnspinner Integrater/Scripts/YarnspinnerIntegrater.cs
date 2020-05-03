4c150f9f424b3cfd5697fadb247bf108848c9ce8
Nomad <netherchef@gmail.com>
2020-05-03 22:05:01 +0800

    Add [Yarnspinner Integrater]

---
+++ CodeBlocks_Lab/Assets/Modules/Yarnspinner Integrater/Scripts/YarnspinnerIntegrater.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YarnspinnerIntegrater : MonoBehaviour
+{
+	// Components
+
+	public Yarn.Unity.DialogueRunner dialogueRunner;
+	public Yarn.Unity.DialogueUI dialogueUI;
+
+	public YarnProgram yarnProgram;
+
+	// Variables
+
+	public bool start;
+	public bool proceed;
+
+	private void Start ()
+	{
+		dialogueRunner.Add (yarnProgram);
+	}
+
+	private void Update ()
+	{
+		if (start)
+		{
+			start = false;
+
+			if (!dialogueRunner.IsDialogueRunning)
+			{
+				dialogueRunner.StartDialogue ();
+			}
+		}
+
+		if (dialogueRunner.IsDialogueRunning)
+		{
+			if (proceed)
+			{
+				proceed = false;
+
+				dialogueUI.MarkLineComplete ();
+			}
+		}
+	}
+}
