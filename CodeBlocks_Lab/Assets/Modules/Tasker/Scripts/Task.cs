ebb2375b6e8d0a5a541f4382ec49dab373076da2
Nomad <netherchef@gmail.com>
2020-05-03 16:54:59 +0800

    Add [Tasker]

---
+++ CodeBlocks_Lab/Assets/Modules/Tasker/Scripts/Task.cs
@@ -0,0 +1,10 @@
+﻿[System.Serializable]
+public struct Task
+{
+	public string name;
+
+	public bool active;
+	public bool complete;
+
+	public bool requireA, requireB, requireC;
+}
---
+++ CodeBlocks_Lab/Assets/Modules/Tasker/Scripts/Tasker.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tasker : MonoBehaviour
+{
+	 //Components
+	
+	[Header ("Check:")]
+	public Task currentTask;
+	public bool checking;
+
+	[Header ("Create:")]
+	public Task newTask;
+	public bool createTask;
+
+	private void Update ()
+	{
+		if (currentTask.active)
+		{
+			// Check Task
+
+			checking = true;
+
+			if (currentTask.requireA || currentTask.requireB || currentTask.requireC)
+			{
+				return;
+			}
+			else
+			{
+				currentTask.active = false;
+				currentTask.complete = true;
+
+				checking = false;
+			}
+		}
+		else if (createTask)
+		{
+			// Create Task
+
+			createTask = false;
+
+			currentTask = newTask;
+		}
+
+
+	}
+}
