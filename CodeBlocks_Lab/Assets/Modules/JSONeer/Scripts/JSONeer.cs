a9179a3788aa18e72a5a8a3fd4e5e5a38eab684f
Nomad <netherchef@gmail.com>
2020-05-03 17:32:57 +0800

    Update [JSONeer]

---
+++ CodeBlocks_Lab/Assets/Modules/JSONeer/Scripts/JSONeer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+[System.Serializable]
+public class JSON_Data
+{
+	public string s;
+}
+
+public class JSONeer : MonoBehaviour
+{
+	private void Start ()
+	{
+		string path = Application.persistentDataPath + "/DataList.json";
+
+		string jsonContent = File.ReadAllText (path);
+
+		JSON_Data d = JsonUtility.FromJson<JSON_Data> (jsonContent);
+
+		print (d.s);
+	}
+}
