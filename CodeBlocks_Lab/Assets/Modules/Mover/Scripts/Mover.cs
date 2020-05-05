602226c90c285ed4287c361ddb4d1d1f5fb22128
Nomad <netherchef@gmail.com>
2020-05-06 03:43:30 +0800

    Add [Mover]

---
+++ CodeBlocks_Lab/Assets/Modules/Mover/Scripts/Mover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+	// Components
+
+	public Transform subject;
+
+	// Variables
+
+	public float speed = 1;
+
+	private void Update ()
+	{
+		if (Input.GetAxisRaw ("Horizontal") != 0)
+		{
+			subject.Translate (new Vector2 (Mathf.Sign (Input.GetAxisRaw ("Horizontal")), 0) * speed * Time.deltaTime);
+		}
+	}
+}
