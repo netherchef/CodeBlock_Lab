538af3a05a816656ff29b0648bee5a1998bb54c8
Nomad <netherchef@gmail.com>
2020-05-07 00:36:33 +0800

    Update [JSONeer]
    Code cleanup with "return default"!

--- CodeBlocks_Lab/Assets/Modules/JSONeer/Scripts/JSONeer.cs
+++ CodeBlocks_Lab/Assets/Modules/JSONeer/Scripts/JSONeer.cs
@@ -1,4 +1,4 @@
-﻿// JSONeer v0.015
+﻿// JSONeer v0.016
 
 using System.Collections;
 using System.Collections.Generic;
@@ -30,8 +30,6 @@ public class JSONeer : MonoBehaviour
 
 	public DataContainer DataContainer_From_JSON ()
 	{
-		DataContainer output = new DataContainer ();
-
 		// Define JSON file path
 
 		string filePath = Application.persistentDataPath + "/" + fileName;
@@ -42,18 +40,25 @@ public class JSONeer : MonoBehaviour
 
 			string jsonContent = File.ReadAllText (filePath);
 
-			// Convert the string from JSON and load it into a struct
+			try
+			{
+				// Convert the string from JSON and load it into a struct
 
-			output = JsonUtility.FromJson<DataContainer> (jsonContent);
+				return JsonUtility.FromJson<DataContainer> (jsonContent);
+			}
+			catch
+			{
+				Debug.LogWarning ("JSON parsing failed.");
+
+				return default;
+			}
 		}
 		catch
 		{
 			Debug.LogWarning ("JSON file failed to load.");
 
-			return output;
+			return default;
 		}
-
-		return output;
 	}
 
 	#endregion
