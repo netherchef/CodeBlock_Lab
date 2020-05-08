fe6f174032b2049add579432f15416e5d79348a8
Nomad <netherchef@gmail.com>
2020-05-09 04:41:31 +0800

    Update [Tasker1 v0.016]
    Task updating working!

--- CodeBlocks_Lab/Assets/Modules/Tasker/Scripts/Tasker1/Tasker1.cs
+++ CodeBlocks_Lab/Assets/Modules/Tasker/Scripts/Tasker1/Tasker1.cs
@@ -39,9 +39,9 @@ public class Tasker1 : MonoBehaviour
 		{
 			checkReqs = false;
 
-			if (Reqs_Fulfilled (targetTask))
+			if (Are_Reqs_Fulfilled (targetTask))
 			{
-				Remove_Task (targetTask);
+				Remove_ActiveTask (targetTask);
 			}
 		}
 	}
@@ -62,6 +62,8 @@ public class Tasker1 : MonoBehaviour
 	//	return false;
 	//}
 
+	#region Begin Task _________________________________________________________
+
 	private void Begin (string taskName)
 	{
 		if (taskName == "")
@@ -70,7 +72,7 @@ public class Tasker1 : MonoBehaviour
 
 			return;
 		}
-		else if (taskName == Find_ActiveTask (taskName).name)
+		else if (Task_Active (taskName))
 		{
 			Debug.LogWarning ("Task already active.");
 
@@ -91,14 +93,11 @@ public class Tasker1 : MonoBehaviour
 		}
 	}
 
-	private void Add_To_ActiveTasks (Task1 task)
-	{
-		// Add task to Active Task List
+	#endregion
 
-		activeTasks.Add (task);
-	}
+	#region Check Task _________________________________________________________
 
-	private bool Reqs_Fulfilled (string taskName)
+	private bool Are_Reqs_Fulfilled (string taskName)
 	{
 		// Are the active task's variables identical to its library equivalent?
 
@@ -108,16 +107,55 @@ public class Tasker1 : MonoBehaviour
 		return Equals (activeTask, libraryTask);
 	}
 
+	#endregion
+
+	#region Update Task ________________________________________________________
+
+	public void Fulfill_Task_Req (string taskName, bool a, bool b, bool c, bool d, bool e)
+	{
+		// Find the task in the active list and make a copy
+
+		Task1 activeTask = Find_ActiveTask (taskName);
+
+		// Compare the input parameters with their equivalent in the copy.
+		// If a match is found, log a warning and return.
+
+		if (activeTask.a || activeTask.b || activeTask.c || activeTask.d || activeTask.e)
+		{
+			Debug.LogWarning (taskName + " | One or more requirements specified already added. Returning...");
+		}
+
+		// Update its values, remove the original from the active list, and put the copy back in.
+
+		activeTask.a = a;
+		activeTask.b = b;
+		activeTask.c = c;
+		activeTask.d = d;
+		activeTask.e = e;
+
+		Remove_ActiveTask (activeTask.name);
+
+		Add_To_ActiveTasks (activeTask);
+	}
+
+	#endregion
+
+	#region Active _____________________________________________________________
+
+	private void Add_To_ActiveTasks (Task1 task)
+	{
+		// Add task to Active Task List
+
+		activeTasks.Add (task);
+	}
+
 	private Task1 Find_ActiveTask (string taskName)
 	{
 		// Look through active tasks and return the task with a matching name
 
 		for (int t = 0; t < activeTasks.Count; t++)
 		{
-			if (activeTasks[t].name == taskName)
-			{
-				return activeTasks[t];
-			}
+			if (activeTasks[t].name == taskName) return activeTasks[t];
 		}
 
 		// If no matching task is found, log warning
@@ -127,16 +165,25 @@ public class Tasker1 : MonoBehaviour
 		return default;
 	}
 
+	private bool Task_Active (string taskName)
+	{
+		for (int t = 0; t < activeTasks.Count; t++)
+		{
+			if (activeTasks[t].name == taskName) return true;
+		}
+
+		return false;
+	}
+
+	#endregion
+
 	private Task1 Get_Task_From_Library (string taskName)
 	{
 		// Look through active tasks and return the task with a matching name
 
 		for (int t = 0; t < taskLibrary.tasks.Count; t++)
 		{
-			if (taskLibrary.tasks[t].name == taskName)
-			{
-				return taskLibrary.tasks[t];
-			}
+			if (taskLibrary.tasks[t].name == taskName) return taskLibrary.tasks[t];
 		}
 
 		// If no matching task is found, log warning
@@ -146,7 +193,7 @@ public class Tasker1 : MonoBehaviour
 		return default;
 	}
 
-	private void Remove_Task (string taskName)
+	private void Remove_ActiveTask (string taskName)
 	{
 		// Look through active tasks and remove the task with a matching name
 
---
+++ CodeBlocks_Lab/Assets/Modules/Tasker/Scripts/Tasker1/Tasker1_Participant.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tasker1_Participant : MonoBehaviour
+{
+	// Components
+
+	public Tasker1 tasker;
+
+	// Variables
+
+	public string taskName;
+
+	private void OnTriggerEnter2D (Collider2D collision)
+	{
+		tasker.Fulfill_Task_Req (taskName, true, false, false, false, false);
+	}
+}
