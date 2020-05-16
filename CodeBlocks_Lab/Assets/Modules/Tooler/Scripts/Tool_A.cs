57bf4331d5621787f6183106d18b7bca98c9f54a
Nomad <netherchef@gmail.com>
2020-05-17 00:10:39 +0800

    Add [Tooler v0.01]

---
+++ CodeBlocks_Lab/Assets/Modules/Tooler/Scripts/Tool_A.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tool_A : MonoBehaviour
+{
+	public void Use ()
+	{
+		print ("HIYA");
+	}
+}
---
+++ CodeBlocks_Lab/Assets/Modules/Tooler/Scripts/Tooler.cs
@@ -0,0 +1,47 @@
+﻿// Tooler v0.01
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ToolType
+{
+	A, B
+}
+
+public class Tooler : MonoBehaviour
+{
+	// Components
+
+	public Tool_A toolA;
+	public Tool_B tool_B;
+
+	// Variables
+
+	public ToolType toolType;
+
+	public bool useTool;
+
+	private void Update ()
+	{
+		if (useTool)
+		{
+			useTool = false;
+
+			Use_Tool ();
+		}
+	}
+
+	private void Use_Tool ()
+	{
+		switch (toolType)
+		{
+			case ToolType.A:
+				toolA.Use ();
+				break;
+			case ToolType.B:
+				tool_B.Use ();
+				break;
+		}
+	}
+}
