48d0618de2833e17d874f0ba1912605d206fd275
Nomad <netherchef@gmail.com>
2020-05-29 03:34:04 +0800

    Update [Puzzler]

--- CodeBlocks_Lab/Assets/Modules/Puzzler/Puzzler.cs
+++ CodeBlocks_Lab/Assets/Modules/Puzzler/Puzzler.cs
@@ -4,13 +4,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum PuzzleType { LockAndKey }
+public enum PuzzleType { LockAndKey, AppleInABasket }
 
 public class Puzzler : MonoBehaviour
 {
 	// Components
 
 	public PuzzleType puzzleType;
+
+	// Apples In A Basket
+
+	[Header ("Apples In A Basket")]
+
+	public List<Transform> apples;
+	public Transform basket;
+
+	public float basketRange = 1;
+
+	// Lock And Key
+
+	[Header ("Lock And Key")]
 
 	public Transform Lock;
 	public Transform Key;
@@ -24,8 +37,56 @@ public class Puzzler : MonoBehaviour
 			case PuzzleType.LockAndKey:
 				Start_LockAndKey ();
 				break;
+			case PuzzleType.AppleInABasket:
+				Start_ApplesInABasket ();
+				break;
 		}
 	}
+
+	private IEnumerator ApplesInABasket ()
+	{
+		// While there are apples outside the basket, check the distance between
+		// each apple and the basket.
+
+		bool applesFound = false;
+
+		while (!applesFound)
+		{
+			for (int a = 0; a < apples.Count; a++)
+			{
+				if (Vector3.Magnitude (apples[a].position - basket.position) < basketRange)
+				{
+					// If an apple is close to the basket,
+					// disable it and remove it from the list.
+
+					apples[a].gameObject.SetActive (false);
+					apples.Remove (apples[a]);
+
+					// If there are no more apples to be found, end the puzzle.
+
+					if (apples.Count <= 0) applesFound = true;
+				}
+			}
+
+			yield return null;
+		}
+
+		// The puzzle has ended
+
+		End_ApplesInABasket ();
+	}
+
+	private void Start_ApplesInABasket ()
+	{
+		StartCoroutine ("ApplesInABasket");
+	}
+
+	private void End_ApplesInABasket ()
+	{
+		print ("The apples are in.");
+	}
+
+	#region Lock And Key _______________________________________________________
 
 	private IEnumerator LockAndKey ()
 	{
@@ -52,4 +113,6 @@ public class Puzzler : MonoBehaviour
 	{
 		print ("We're done here.");
 	}
+
+	#endregion
 }
