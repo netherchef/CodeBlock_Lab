adb8b9c91a017d267481d6588d9d455b57ff9f20
Nomad <netherchef@gmail.com>
2020-06-02 00:03:20 +0800

    Update [Animator]
    Added Animator Functions for state handling through code!

---
+++ CodeBlocks_Lab/Assets/Modules/Animator/AnimatorFunctions.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorFunctions : MonoBehaviour
+{
+	// Components
+
+	public Animator animator;
+
+	private int animHash_Moving = Animator.StringToHash ("Moving");
+	private int animHash_FacingRight = Animator.StringToHash ("FacingRight");
+
+	private bool Is_Moving ()
+	{
+		return animator.GetBool (animHash_Moving);
+	}
+
+	private void Set_Moving (bool state)
+	{
+		animator.SetBool (animHash_Moving, state);
+	}
+
+	private bool Is_FacingRight ()
+	{
+		return animator.GetBool (animHash_FacingRight);
+	}
+
+	private void Set_FacingRight (bool state)
+	{
+		animator.SetBool (animHash_FacingRight, state);
+	}
+}
