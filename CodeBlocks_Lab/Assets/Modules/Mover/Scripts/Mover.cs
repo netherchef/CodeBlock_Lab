47d3546beaddc5e66b8b80ec766d4de52939a026
Nomad <netherchef@gmail.com>
2020-06-25 13:00:02 +0800

    Update [Mover]
    Mover v0.02 now has 2 types: Player Mover & NPC Mover.

--- CodeBlocks_Lab/Assets/Modules/Mover/Scripts/Mover.cs
+++ CodeBlocks_Lab/Assets/Modules/Mover/Scripts/Mover.cs
@@ -1,8 +1,11 @@
-﻿// Mover v0.01
+﻿// Mover v0.02
 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+public enum MoverType { Player, NPC }
+public enum MoveState { Stop, Left, Right }
 
 public class Mover : MonoBehaviour
 {
@@ -12,13 +15,111 @@ public class Mover : MonoBehaviour
 
 	// Variables
 
+	public MoverType moverType;
+	public MoveState moveState;
 	public float speed = 1;
 
-	private void Update ()
+	// Enumerators
+
+	private IEnumerator playerMover;
+	private IEnumerator nPCMover;
+
+	private void Start ()
 	{
-		if (Input.GetAxisRaw ("Horizontal") != 0)
+		switch (moverType)
 		{
-			subject.Translate (new Vector2 (Mathf.Sign (Input.GetAxisRaw ("Horizontal")), 0) * speed * Time.deltaTime);
+			case MoverType.Player:
+				playerMover = PlayerMover ();
+				StartCoroutine (playerMover);
+				break;
+
+			case MoverType.NPC:
+				nPCMover = NPCMover ();
+				StartCoroutine (nPCMover);
+				break;
 		}
 	}
+
+	#region Mover Types ________________________________________________________
+
+	private IEnumerator PlayerMover ()
+	{
+		while (enabled)
+		{
+			if (Input.GetAxisRaw ("Horizontal") != 0)
+			{
+				if (Input.GetAxisRaw ("Horizontal") < 0)
+					Move_Left ();
+				else if (Input.GetAxisRaw ("Horizontal") > 0)
+					Move_Right ();
+			}
+			else
+			{
+				if (Get_MoveState () != MoveState.Stop) Stop ();
+
+				yield return null;
+			}
+
+			Move ();
+
+			yield return null;
+		}
+	}
+
+	private IEnumerator NPCMover ()
+	{
+		while (enabled)
+		{
+			Move ();
+
+			yield return null;
+		}
+	}
+
+	#endregion
+
+	#region Move _______________________________________________________________
+
+	private void Move ()
+	{
+		switch (moveState)
+		{
+			case MoveState.Stop:
+				return;
+
+			case MoveState.Left:
+				subject.Translate (new Vector2 (-1, 0) * speed * Time.deltaTime);
+				break;
+
+			case MoveState.Right:
+				subject.Translate (new Vector2 (1, 0) * speed * Time.deltaTime);
+				break;
+		}
+	}
+
+	#endregion
+
+	#region Move States ________________________________________________________
+
+	public void Stop ()
+	{
+		moveState = MoveState.Stop;
+	}
+
+	public void Move_Left ()
+	{
+		moveState = MoveState.Left;
+	}
+
+	public void Move_Right ()
+	{
+		moveState = MoveState.Right;
+	}
+
+	private MoveState Get_MoveState ()
+	{
+		return moveState;
+	}
+
+	#endregion
 }
