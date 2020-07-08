4daa3b568cd403b1b95461bcf888ff3725743973
Nomad <netherchef@gmail.com>
2020-07-08 13:42:48 +0800

    Update [Planter v0.01]
    Planter now supports multiple types!

--- CodeBlocks_Lab/Assets/Modules/Planter/Planter.cs
+++ CodeBlocks_Lab/Assets/Modules/Planter/Planter.cs
@@ -4,6 +4,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public struct Clone
+{
+	public string modeName;
+	public GameObject prefab;
+	public KeyCode hotkey;
+}
+
 public class Planter : MonoBehaviour
 {
 	// Components
@@ -12,19 +20,44 @@ public class Planter : MonoBehaviour
 
 	// Variables
 
-	public GameObject clone;
+	public Clone[] clones;
+
+	public string currentMode = "Box";
 
 	private void Update ()
 	{
+		// Plant
+
 		if (Input.GetMouseButtonDown (0))
 		{
-			Plant ();
+			for (int i = 0; i < clones.Length; i++)
+			{
+				if (clones[i].modeName == currentMode)
+				{
+					Plant (clones[i].prefab, Plant_Position ());
+				}
+			}
+
+			return;
+		}
+
+		// Mode Change
+
+		if (Input.anyKeyDown)
+		{
+			for (int c = 0; c < clones.Length; c++)
+			{
+				if (Input.GetKeyDown (clones[c].hotkey))
+				{
+					currentMode = clones[c].modeName;
+				}
+			}
 		}
 	}
 
-	private void Plant ()
+	private void Plant (GameObject prefab, Vector3 pos)
 	{
-		Instantiate (clone, Plant_Position (), Quaternion.identity, transform);
+		Instantiate (prefab, pos, Quaternion.identity, transform);
 	}
 
 	private Vector2 Plant_Position ()
