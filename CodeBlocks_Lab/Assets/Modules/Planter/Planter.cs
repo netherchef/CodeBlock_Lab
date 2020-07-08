d22ca79bfcb079f035a44f7c76ae3b4418ac6145
Nomad <netherchef@gmail.com>
2020-07-09 03:10:23 +0800

    Update [Planter v0.01]
    1. New text to display current mode
    2. Clone preview shows at mouse position

--- CodeBlocks_Lab/Assets/Modules/Planter/Planter.cs
+++ CodeBlocks_Lab/Assets/Modules/Planter/Planter.cs
@@ -3,12 +3,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [System.Serializable]
 public struct Clone
 {
 	public string modeName;
-	public GameObject prefab;
+	public GameObject[] prefabs;
 	public KeyCode hotkey;
 }
 
@@ -17,12 +18,33 @@ public class Planter : MonoBehaviour
 	// Components
 
 	public Camera cam;
+	public Text modeDisplay;
+
+	private GameObject currentClone;
 
 	// Variables
 
 	public Clone[] clones;
 
-	public string currentMode = "Box";
+	private string currentMode = "Box";
+
+	private void Start ()
+	{
+		if (!currentClone)
+		{
+			for (int c = 0; c < clones.Length; c++)
+			{
+				if (clones[c].modeName == currentMode)
+				{
+					// Spawn clone
+
+					GameObject clone = Instantiate (clones[c].prefabs[0], MousePosition (), Quaternion.identity, transform);
+
+					currentClone = clone;
+				}
+			}
+		}
+	}
 
 	private void Update ()
 	{
@@ -30,13 +52,7 @@ public class Planter : MonoBehaviour
 
 		if (Input.GetMouseButtonDown (0))
 		{
-			for (int i = 0; i < clones.Length; i++)
-			{
-				if (clones[i].modeName == currentMode)
-				{
-					Plant (clones[i].prefab, Plant_Position ());
-				}
-			}
+			Instantiate (currentClone, MousePosition (), Quaternion.identity, transform);
 
 			return;
 		}
@@ -49,18 +65,35 @@ public class Planter : MonoBehaviour
 			{
 				if (Input.GetKeyDown (clones[c].hotkey))
 				{
+					// Change the current mode
+
 					currentMode = clones[c].modeName;
+
+					// Update the mode display
+
+					modeDisplay.text = currentMode;
+
+					// Destroy current clone
+
+					Destroy (currentClone);
+
+					// Spawn clone
+
+					GameObject clone = Instantiate (clones[c].prefabs[0], MousePosition (), Quaternion.identity, transform);
+
+					currentClone = clone;
 				}
 			}
+
+			return;
 		}
+
+		// Position clone at mouse
+
+		if (currentClone) currentClone.transform.position = MousePosition ();
 	}
 
-	private void Plant (GameObject prefab, Vector3 pos)
-	{
-		Instantiate (prefab, pos, Quaternion.identity, transform);
-	}
-
-	private Vector2 Plant_Position ()
+	private Vector2 MousePosition ()
 	{
 		Vector3 mousePos = cam.ScreenToWorldPoint (Input.mousePosition);
 		return new Vector2 (mousePos.x, mousePos.y);
