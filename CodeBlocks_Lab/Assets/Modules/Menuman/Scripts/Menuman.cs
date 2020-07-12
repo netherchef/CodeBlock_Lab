8943e26ba3a40ea669af47e0a92cdca65b22e650
Nomad <netherchef@gmail.com>
2020-07-13 01:45:50 +0800

    Add [Menuman]
    For menus.

---
+++ CodeBlocks_Lab/Assets/Modules/Menuman/Scripts/Menuman.cs
@@ -0,0 +1,34 @@
+﻿// Menuman v0.01
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menuman : MonoBehaviour
+{
+	// Components
+
+	public GameObject[] buttons;
+
+	// Variables
+
+	public int currentButtonIndex;
+
+	private void Update ()
+	{
+		if (Input.GetAxisRaw ("Vertical") > 0)
+		{
+			if (currentButtonIndex > 0)
+			{
+				currentButtonIndex--;
+			}
+		}
+		else if (Input.GetAxisRaw ("Vertical") < 0)
+		{
+			if (currentButtonIndex < buttons.Length - 1)
+			{
+				currentButtonIndex++;
+			}
+		}
+	}
+}
