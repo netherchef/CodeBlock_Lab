a8b1428bc98669a96fd709245ac38fec09ebe2e7
Nomad <netherchef@gmail.com>
2020-07-20 19:44:43 +0800

    Update [ImageFader]

--- CodeBlocks_Lab/Assets/Modules/Fader/Scripts/ImageFader.cs
+++ CodeBlocks_Lab/Assets/Modules/Fader/Scripts/ImageFader.cs
@@ -12,44 +12,79 @@ public class ImageFader : MonoBehaviour
 	// Components
 
 	public Image image;
-	public FadeMode fadeMode;
+	public FadeMode_Sprite fadeMode;
 
 	// Variables
 
-	public bool fade;
+	public bool startFade;
 
 	public float speed = 2;
 
+	// Enumerators
+
+	private IEnumerator doFade;
+
 	private void Update ()
 	{
-		if (fade)
+		if (startFade)
 		{
-			if (fadeMode == FadeMode.Out)
-			{
-				if (image.color.a > 0)
-				{
-					image.color -= new Color (0, 0, 0, speed * Time.deltaTime);
-				}
-				else
-				{
-					image.color = new Color (image.color.r, image.color.g, image.color.b, 0);
-				}
+			startFade = false;
 
-				return;
-			}
-			else if (fadeMode == FadeMode.In)
-			{
-				if (image.color.a < 1)
-				{
-					image.color += new Color (0, 0, 0, speed * Time.deltaTime);
-				}
-				else
-				{
-					image.color = new Color (image.color.r, image.color.g, image.color.b, 1);
-				}
-
-				return;
-			}
+			Fade (fadeMode);
 		}
+	}
+
+	#region Colour _____________________________________________________________
+
+	private void Set_Image_Alpha (Image img, float alpha)
+	{
+		img.color = new Color (img.color.r, img.color.g, img.color.b, alpha);
+	}
+
+	#endregion
+
+	private void Fade (FadeMode_Sprite mode)
+	{
+		if (doFade != null) StopCoroutine (doFade);
+
+		if (mode == FadeMode_Sprite.In)
+		{
+			doFade = Do_Fade_In ();
+			StartCoroutine (doFade);
+
+			return;
+		}
+
+		if (mode == FadeMode_Sprite.Out)
+		{
+			doFade = Do_Fade_Out ();
+			StartCoroutine (doFade);
+
+			return;
+		}
+	}
+
+	private IEnumerator Do_Fade_In ()
+	{
+		while (image.color.a < 1)
+		{
+			image.color += new Color (0, 0, 0, speed * Time.deltaTime);
+
+			yield return null;
+		}
+
+		Set_Image_Alpha (image, 1);
+	}
+
+	private IEnumerator Do_Fade_Out ()
+	{
+		while (image.color.a > 0)
+		{
+			image.color -= new Color (0, 0, 0, speed * Time.deltaTime);
+
+			yield return null;
+		}
+
+		Set_Image_Alpha (image, 0);
 	}
 }
