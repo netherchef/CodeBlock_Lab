8cf93a4fb1b8316b39cf6b532b7a3a7021bd705f
Nomad <netherchef@gmail.com>
2020-07-30 22:06:19 +0800

    Add [Shotter]

---
+++ CodeBlocks_Lab/Assets/Modules/OneShotter/Scripts/Shotter.cs
@@ -0,0 +1,81 @@
+﻿// Shotter v0.01
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayMode { Single, Repeat }
+
+public class Shotter : MonoBehaviour
+{
+	// Components
+
+	private SpriteRenderer spriteRenderer;
+
+	// Variables
+
+	public PlayMode playMode;
+
+	public Sprite[] sprites;
+	public float interval = 0.5f;
+	private int currentSpriteIndex;
+	private float lastUpdateTime;
+
+	private void Start ()
+	{
+		spriteRenderer = gameObject.AddComponent<SpriteRenderer> ();
+	}
+
+	private void Update ()
+	{
+		if (playMode == PlayMode.Single)
+		{
+			if (currentSpriteIndex < sprites.Length)
+			{
+				if (Time.time > lastUpdateTime + interval)
+				{
+					spriteRenderer.sprite = sprites[currentSpriteIndex];
+
+					currentSpriteIndex++;
+
+					lastUpdateTime = Time.time;
+				}
+			}
+			else
+			{
+				if (Time.time > lastUpdateTime + interval)
+				{
+					Destroy (this.gameObject);
+				}
+			}
+
+			return;
+		}
+
+		if (playMode == PlayMode.Repeat)
+		{
+			if (currentSpriteIndex < sprites.Length)
+			{
+				if (Time.time > lastUpdateTime + interval)
+				{
+					spriteRenderer.sprite = sprites[currentSpriteIndex];
+
+					currentSpriteIndex++;
+
+					lastUpdateTime = Time.time;
+				}
+
+				return;
+			}
+
+			if (Time.time > lastUpdateTime + interval)
+			{
+				currentSpriteIndex = 0;
+
+				spriteRenderer.sprite = sprites[0];
+
+				lastUpdateTime = Time.time;
+			}
+		}
+	}
+}
