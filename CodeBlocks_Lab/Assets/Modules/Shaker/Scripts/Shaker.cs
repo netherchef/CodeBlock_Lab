437fb35238142693442a0a35c6fc5e9c970a52ea
Nomad <netherchef@gmail.com>
2020-12-19 18:44:23 +0800

    Add [Shaker.cs]

---
+++ CodeBlocks_Lab/Assets/Modules/Shaker/Scripts/Shaker.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shaker : MonoBehaviour
+{
+	// Components
+
+	public Transform targetContainer;
+	public Transform target;
+
+	// Enumerators
+
+	private IEnumerator do_Shake;
+
+	// Debug
+
+	[Header ("Debug:")]
+	public bool debug;
+	public bool start;
+
+	public float debugDuration = 1f;
+	public float debugPower = 1f;
+	public float debugLossRate = 0f;
+
+	private void Update ()
+	{
+		if (start)
+		{
+			start = false;
+
+			Start_Shake (debugDuration, debugPower, debugLossRate);
+		}
+	}
+
+	private IEnumerator Do_Shake (float duration = 1f, float power = 1f, float lossRate = 0f)
+	{
+		float pow = power;
+
+		float endTime = Time.time + duration;
+
+		while (Time.time < endTime)
+		{
+			// Calculate Power Loss
+
+			if (pow > 0) pow -= lossRate;
+
+			// Calculate Displacement
+
+			Vector3 displacement = Random.insideUnitCircle * pow;
+
+			// Determine Final Position
+
+			Vector3 finalPos = new Vector3 (targetContainer.position.x + displacement.x, targetContainer.position.y + displacement.y, target.position.z);
+
+			// Position Target at Final Position
+
+			target.position = finalPos;
+
+			yield return null;
+		}
+
+		// Reset Target Position
+
+		target.position = new Vector3 (targetContainer.position.x, targetContainer.position.y, target.position.z);
+
+		// Reset
+
+		do_Shake = null;
+	}
+
+	private void Start_Shake (float duration = 1f, float power = 1f, float lossRate = 0f)
+	{
+		if (do_Shake != null)
+		{
+			if (debug) Debug.LogWarning ("Do Shake has NOT been cleared.");
+
+			return;
+		}
+
+		do_Shake = Do_Shake (duration, power, lossRate);
+		StartCoroutine (do_Shake);
+	}
+}
