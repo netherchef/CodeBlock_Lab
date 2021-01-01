de969b377ce4164a072372b18861a3138249a73a
Nomad <netherchef@gmail.com>
2021-01-01 18:05:27 +0800

    Add [Intervaler]

---
+++ CodeBlocks_Lab/Assets/Modules/Intervaler/Intervaler.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Intervaler : MonoBehaviour
+{
+	// Variables
+
+	private float timer;
+	private bool active;
+
+	private void Update ()
+	{
+		// If the Submit Button is pressed, activate the timer.
+
+		if (Input.GetButtonDown ("Submit"))
+		{
+			active = true;
+		}
+
+		/// While the timer is active:
+		/// 1. If the Submit button is released withing the allotted time, it's a Light Tap!
+		/// 2. If it's held longer than the allotted time, it's a Normal Press!
+
+		while (active)
+		{
+			timer += Time.deltaTime;
+
+			if (Input.GetButtonUp ("Submit"))
+			{
+				if (timer <= 0.1f)
+				{
+					print ("Light Tap!");
+				}
+
+				timer = 0;
+
+				active = false;
+
+				return;
+			}
+
+			if (timer >= 0.1f)
+			{
+				print ("Normal Press!");
+
+				timer = 0;
+
+				active = false;
+
+				return;
+			}
+
+			return;
+		}
+	}
+}
---
+++ CodeBlocks_Lab/Assets/Modules/Menuman/Scripts/MenuSounder.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSounder : MonoBehaviour
+{
+	// Components
+
+	public AudioSource audioSource;
+
+	public AudioClip hover;
+	public AudioClip select;
+
+	public void Play_Hover_Sound ()
+	{
+		audioSource.PlayOneShot (hover);
+	}
+
+	public void Play_Select_Sound ()
+	{
+		audioSource.PlayOneShot (select);
+	}
+}
